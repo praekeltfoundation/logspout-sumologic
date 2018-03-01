a45eaa31b781721c4219177fe4133f7af326a46f
Jeremy Thurgood <jeremy@praekelt.org>
2018-03-01 15:37:23 +0200

    More HTTP tests.

--- sumologic_test.go
+++ sumologic_test.go
@@ -8,7 +8,9 @@ import (
 	"net/http/httptest"
 	"os"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	docker "github.com/fsouza/go-dockerclient"
 	"github.com/gliderlabs/logspout/router"
@@ -92,23 +94,42 @@ type RequestData struct {
 }
 
 // FakeSumo starts a fake Sumo Logic server that expects a sequence of requests
-// matching the provided RequestData slice.
-func (ts *TestSuite) FakeSumo(expectedRequestData []RequestData) *httptest.Server {
-	requestIndex := 0
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		erd := expectedRequestData[requestIndex]
-		requestIndex++
-		ts.Equal(erd.Headers, extractExpectedHeaders(erd.Headers, r.Header))
-		body := ts.ReadJSON(r.Body)
-		// NOTE: This mutates the body map because it's too hard not to. :-(
-		if erd.Body["timestamp"] == nil {
-			erd.Body["timestamp"] = body["timestamp"]
-		}
-		ts.Equal(erd.Body, body)
-	})
+// matching the provided RequestData slice, then returns an Adapter pointing at
+// that server and a function that returns the number of requests consumed so
+// far.
+func (ts *TestSuite) FakeSumo(expectedRequestData []RequestData) (*Adapter, func() int) {
+	handler, counter := ts.mkHandler(expectedRequestData)
 	server := httptest.NewServer(handler)
 	ts.AddCleanup(server.Close)
-	return server
+
+	return ts.WithoutError(NewAdapter(&router.Route{
+		ID:      "foo",
+		Address: server.URL,
+		Adapter: "sumologic",
+	})).(*Adapter), counter
+}
+
+func (ts *TestSuite) mkHandler(expectedRequestData []RequestData) (http.Handler, func() int) {
+	// *sigh* shared-memory concurrency.
+	mux := sync.Mutex{}
+	requestIndex := 0
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mux.Lock()
+			erd := expectedRequestData[requestIndex]
+			requestIndex++
+			mux.Unlock()
+			ts.Equal(erd.Headers, extractExpectedHeaders(erd.Headers, r.Header))
+			body := ts.ReadJSON(r.Body)
+			// NOTE: This mutates the body map because it's too hard not to. :-(
+			if erd.Body["timestamp"] == nil {
+				erd.Body["timestamp"] = body["timestamp"]
+			}
+			ts.Equal(erd.Body, body)
+		}), func() int {
+			mux.Lock()
+			defer mux.Unlock()
+			return requestIndex
+		}
 }
 
 func extractExpectedHeaders(
@@ -324,6 +345,28 @@ func (ts *TestSuite) Test_buildHeaders_with_all_fields() {
 	ts.Equal(expectedHeaders, headers)
 }
 
+func (ts *TestSuite) Test_sendLog_empty_message() {
+	expectedRequestData := []RequestData{
+		{
+			Headers: map[string]string{
+				"X-Sumo-Name": "",
+				"X-Sumo-Host": "",
+			},
+			Body: mkExpectedBody(jsonobj{}),
+		},
+	}
+	adapter, requestCounter := ts.FakeSumo(expectedRequestData)
+
+	msg := &router.Message{
+		Container: &docker.Container{
+			Config: &docker.Config{},
+		},
+	}
+
+	adapter.sendLog(msg)
+	ts.Equal(len(expectedRequestData), requestCounter())
+}
+
 func (ts *TestSuite) Test_sendLog_simple_message() {
 	expectedRequestData := []RequestData{
 		{
@@ -331,26 +374,16 @@ func (ts *TestSuite) Test_sendLog_simple_message() {
 				"X-Sumo-Name": "box",
 				"X-Sumo-Host": "example.com",
 			},
-			Body: jsonobj{
+			Body: mkExpectedBody(jsonobj{
 				"message": "Some data.",
 				"container": jsonobj{
 					"docker_name":     "box",
 					"docker_hostname": "example.com",
-					"docker_id":       "",
-					"docker_image":    "",
-					"time":            "0001-01-01T00:00:00Z",
-					"source":          "",
 				},
-			},
+			}),
 		},
 	}
-	server := ts.FakeSumo(expectedRequestData)
-
-	adapter := ts.WithoutError(NewAdapter(&router.Route{
-		ID:      "foo",
-		Address: server.URL,
-		Adapter: "sumologic",
-	})).(*Adapter)
+	adapter, requestCounter := ts.FakeSumo(expectedRequestData)
 
 	msg := &router.Message{
 		Data: "Some data.",
@@ -363,4 +396,122 @@ func (ts *TestSuite) Test_sendLog_simple_message() {
 	}
 
 	adapter.sendLog(msg)
+	ts.Equal(len(expectedRequestData), requestCounter())
+}
+
+func (ts *TestSuite) Test_Stream_empty_message() {
+	expectedRequestData := []RequestData{
+		{
+			Headers: map[string]string{
+				"X-Sumo-Name": "",
+				"X-Sumo-Host": "",
+			},
+			Body: mkExpectedBody(jsonobj{}),
+		},
+	}
+	adapter, requestCounter := ts.FakeSumo(expectedRequestData)
+
+	ch := make(chan *router.Message)
+	go adapter.Stream(ch)
+
+	ch <- &router.Message{
+		Container: &docker.Container{
+			Config: &docker.Config{},
+		},
+	}
+
+	close(ch)
+	// Sleep a bit so async sendLog()s can finish.
+	time.Sleep(100 * time.Millisecond)
+	ts.Equal(len(expectedRequestData), requestCounter())
+}
+
+func (ts *TestSuite) Test_Stream_two_messages() {
+	expectedRequestData := []RequestData{
+		{
+			Headers: map[string]string{
+				"X-Sumo-Name": "",
+				"X-Sumo-Host": "",
+			},
+			Body: mkExpectedBody(jsonobj{}),
+		},
+		{
+			Headers: map[string]string{
+				"X-Sumo-Name": "box",
+				"X-Sumo-Host": "example.com",
+			},
+			Body: mkExpectedBody(jsonobj{
+				"message": "Some data.",
+				"container": jsonobj{
+					"docker_name":     "box",
+					"docker_hostname": "example.com",
+				},
+			}),
+		},
+	}
+	adapter, requestCounter := ts.FakeSumo(expectedRequestData)
+
+	ch := make(chan *router.Message)
+	go adapter.Stream(ch)
+
+	ch <- &router.Message{
+		Container: &docker.Container{
+			Config: &docker.Config{},
+		},
+	}
+
+	ch <- &router.Message{
+		Data: "Some data.",
+		Container: &docker.Container{
+			Name: "box",
+			Config: &docker.Config{
+				Hostname: "example.com",
+			},
+		},
+	}
+
+	close(ch)
+	// Sleep a bit so async sendLog()s can finish.
+	time.Sleep(100 * time.Millisecond)
+	ts.Equal(len(expectedRequestData), requestCounter())
+}
+
+// Some HTTP test helper things.
+
+func getobj(obj jsonobj, k string) jsonobj {
+	result, present := obj[k]
+	if present {
+		return result.(jsonobj)
+	}
+	return jsonobj{}
+}
+
+func mergeJSONs(objs ...jsonobj) jsonobj {
+	result := jsonobj{}
+	for _, obj := range objs {
+		for k, v := range obj {
+			switch v.(type) {
+			case jsonobj:
+				result[k] = mergeJSONs(getobj(result, k), v.(jsonobj))
+			default:
+				result[k] = v
+			}
+		}
+	}
+	return result
+}
+
+func mkExpectedBody(fields jsonobj) jsonobj {
+	dfault := jsonobj{
+		"message": "",
+		"container": jsonobj{
+			"docker_name":     "",
+			"docker_hostname": "",
+			"docker_id":       "",
+			"docker_image":    "",
+			"time":            "0001-01-01T00:00:00Z",
+			"source":          "",
+		},
+	}
+	return mergeJSONs(dfault, fields)
 }
